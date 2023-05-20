1c65b6c8551ade167f54d7605619b992c62c874e
Cain <75994858+cainthebest@users.noreply.github.com>
2023-05-20 01:00:37 +0100

    impl: query

---
+++ crates/gstat-core/src/traits/query.rs
@@ -0,0 +1,23 @@
+use crate::Error;
+
+use std::error::Error as StdError;
+
+/// A `Query` trait represents a type that can be instantiated and then sent to a protocol.
+///
+/// This trait is generic over the type of Query Error `E`.
+pub trait Query: Sized {
+    /// The type for query errors.
+    type E: StdError + 'static;
+
+    /// Creates a new instance of the Query.
+    ///
+    /// This method is expected to return a `Result` containing the newly created
+    /// `Query` or an `Error` if the instantiation fails.
+    ///
+    /// This query can later be used with a protocol to send a request or a command.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing either a new instance of the Query or an `Error`.
+    fn new() -> Result<Self, Error<Self::E>>;
+}
