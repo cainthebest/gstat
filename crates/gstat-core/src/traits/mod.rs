6d9767ac5d7861de99f19c3025081795f995c6c8
Cain <75994858+cainthebest@users.noreply.github.com>
2023-05-20 01:01:07 +0100

    chore: add mod

---
+++ crates/gstat-core/src/traits/mod.rs
@@ -0,0 +1,11 @@
+pub mod game;
+pub mod parser;
+pub mod protocol;
+pub mod query;
+pub mod response;
+
+
+pub use parser::Parser;
+pub use protocol::Protocol;
+pub use query::Query;
+pub use response::Response;
